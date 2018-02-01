6d56c93cb5137a4d23752b5be0c29d493239a715
Vikram Sharma <vsharma8363@gmail.com>
2018-01-31 20:41:50 -0800

    Update QuikCamsStart.sh

--- QuikCams/QuikCamsStart.sh
+++ QuikCams/QuikCamsStart.sh
@@ -1,3 +1,5 @@
-C:\gstreamer\1.0\x86_64\bin\gst-launch-1.0.exe -e -v udpsrc port=5001 ! application/x-rtp, encoding-name=JPEG, payload=26 ! rtpjpegdepay ! jpegdec ! autovideosink
+To open first stream:
+C:\gstreamer\1.0\x86_64\bin\gst-launch-1.0.exe -e -v udpsrc port=1181 ! application/x-rtp, encoding-name=JPEG, payload=26 ! rtpjpegdepay ! jpegdec ! autovideosink
 
- gst-launch-1.0 -v v4l2src ! "image/jpeg,width=320,height=180,framerate=30/1" ! rtpjpegpay ! udpsink host=10.0.0.7 port=5001
+To open second stream:
+C:\gstreamer\1.0\x86_64\bin\gst-launch-1.0.exe -e -v udpsrc port=1182 ! application/x-rtp, encoding-name=JPEG, payload=26 ! rtpjpegdepay ! jpegdec ! autovideosink
