5bee2ac9d8560ed289949c46499a5ac371de87dd
Vikram Sharma <vsharma8363@gmail.com>
2018-02-01 19:07:28 -0800

    Update receiver.bat

--- QuikCams/receiver.bat
+++ QuikCams/receiver.bat
@@ -1,1 +1,2 @@
 start cmd.exe @cmd /k "C:\gstreamer\1.0\x86_64\bin\gst-launch-1.0.exe -e -v udpsrc port=1181 ! application/x-rtp, encoding-name=JPEG, payload=26 ! rtpjpegdepay ! jpegdec ! autovideosink"
+start cmd.exe @cmd /k "C:\gstreamer\1.0\x86_64\bin\gst-launch-1.0.exe -e -v udpsrc port=1182 ! application/x-rtp, encoding-name=JPEG, payload=26 ! rtpjpegdepay ! jpegdec ! autovideosink"
