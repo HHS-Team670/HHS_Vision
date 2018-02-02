d070f2b0b8765a9944b7f215bd4b818a2f0229c8
Vikram Sharma <vsharma8363@gmail.com>
2018-02-01 18:58:25 -0800

    Update runMJPG.sh

--- QuikCams/runMJPG.sh
+++ QuikCams/runMJPG.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
-# usage: ./runMJPG.sh <camera> <drivestation IP or hostname>
+# usage: ./runMJPG.sh <drivestation IP or hostname>
 
-gst-launch-1.0 v4l2src device=/dev/video$1 ! video/x-raw, width=320, height=240, framerate=30/1 ! jpegenc ! rtpjpegpay ! udpsink host=$2 port=1181
+terminal -e gst-launch-1.0 v4l2src device=/dev/video0 ! video/x-raw, width=320, height=240, framerate=30/1 ! jpegenc ! rtpjpegpay ! udpsink host=1181 port=1181
+terminal -e gst-launch-1.0 v4l2src device=/dev/video1 ! video/x-raw, width=320, height=240, framerate=30/1 ! jpegenc ! rtpjpegpay ! udpsink host=1182 port=1181
