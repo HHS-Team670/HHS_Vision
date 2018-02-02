e62090c6f91d0a2696f1137d418b2576fa6bc63e
Vikram Sharma <vsharma8363@gmail.com>
2018-02-01 19:05:16 -0800

    Delete QuikCamsStart.sh

--- QuikCams/QuikCamsStart.sh
+++
@@ -1,5 +0,0 @@
-To open first stream:
-C:\gstreamer\1.0\x86_64\bin\gst-launch-1.0.exe -e -v udpsrc port=1181 ! application/x-rtp, encoding-name=JPEG, payload=26 ! rtpjpegdepay ! jpegdec ! autovideosink
-
-To open second stream:
-C:\gstreamer\1.0\x86_64\bin\gst-launch-1.0.exe -e -v udpsrc port=1182 ! application/x-rtp, encoding-name=JPEG, payload=26 ! rtpjpegdepay ! jpegdec ! autovideosink
