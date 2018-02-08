7e461c7cb057008b7bde74c35366024740a3d6e2
Vikram Sharma <vsharma8363@gmail.com>
2018-02-07 17:13:31 -0800

    Update pynetworksensors.py

--- pynetworksensors.py
+++ pynetworksensors.py
@@ -1,1 +1,15 @@
+import sys
+import time
+from networktables import NetworkTables
 
+ip = "10.6.70.2"
+NetworkTables.initialize(server=ip)
+
+table = NetworkTables.getTable("rpi")
+
+luno = table.getAutoUpdateValue('lidar_uno', -1)
+
+while True:
+    print('lidar_uno:', luno.value)
+    table.putNumber('lidar_uno', 24)
+    time.sleep(1)
