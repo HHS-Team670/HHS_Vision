e2d247910794b0f1ea19a68f643de01c78fdef2c
Vikram Sharma <vsharma8363@gmail.com>
2018-02-07 17:43:04 -0800

    Update pynetworksensors.py

--- pynetworksensors.py
+++ pynetworksensors.py
@@ -2,14 +2,12 @@ import sys
 import time
 from networktables import NetworkTables
 
-ip = "10.6.70.2"
+ip = "roboRIO-670-frc.local"
 NetworkTables.initialize(server=ip)
 
 table = NetworkTables.getTable("rpi")
 
-luno = table.getAutoUpdateValue('lidar_uno', -1)
+lidarUnoVal=0
 
 while True:
-    print('lidar_uno:', luno.value)
-    table.putNumber('lidar_uno', 24)
-    time.sleep(1)
+    table.putNumber('lidar_uno', lidarUnoVal)
