f186c33bf7b39fbb4a296d92935dc824e6f2168f
Vikram Sharma <vsharma8363@gmail.com>
2018-02-14 23:23:26 -0800

    Update installation.sh

--- installation.sh
+++ installation.sh
@@ -7,13 +7,23 @@ echo -e "installing gstreamer"
 sudo apt update
 sudo apt install gstreamer1.0 gstreamer-tools
 
+echo -e "installing PIP for python"
+sudo apt install python-pip
+
+echo -e "updating PIP for python"
+sudo pip install --upgrade pip
+
+echo -e "installing pynetworktables"
+pip install pynetworktables
+
+echo -e "Power cube python opencv software is being installed (do not move this file)"
+sudo mv PowerCuber.py /home/PowerCuber.py
+
 echo -e "setting OGStreamer.sh permission"
 sudo chmod 777 ./launch_ogstreamer.sh
 
-
-echo -e "setting launch_ogstreamer.sh as startup script (do not move this file)"
+echo -e "setting launch_ogstreamer.sh location (do not move this file)"
 sudo mv launch_ogstreamer.sh /home/launch_ogstreamer.sh
 sudo chmod +x /home/launch_ogstreamer.sh
 
-sudo rm -r /etc/rc.local
-sudo mv rc.local /etc/rc.local
+echo -e "Setting startup script......."
