8a69c2be2e1714574fd6461514b49fba901d310c
Vikram Sharma <vsharma8363@gmail.com>
2018-02-14 23:27:55 -0800

    Update launch_ogstreamer.sh

--- launch_ogstreamer.sh
+++ launch_ogstreamer.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-python /home/PowerCuber.py
+python /home/PowerCuber.py &
 gst-launch-1.0 v4l2src device=/dev/video0 ! video/x-raw, width=320, height=240, framerate=30/1 ! jpegenc ! rtpjpegpay ! udpsink host=LAPTOP-LU3PAHOV.local port=1181 &
 gst-launch-1.0 v4l2src device=/dev/video1 ! video/x-raw, width=320, height=240, framerate=30/1 ! jpegenc ! rtpjpegpay ! udpsink host=LAPTOP-LU3PAHOV.local port=1182 &
